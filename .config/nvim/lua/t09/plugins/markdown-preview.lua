38af4715f9376494879647874302ea6e6dc93f16
Testspieler09 <pepehanisch06@gmail.com>
2025-06-04 15:33:06 +0200

    feat: markdown preview

---
+++ .config/nvim/lua/t09/plugins/markdown-preview.lua
@@ -0,0 +1,9 @@
+-- NOTE: if the build fails just run `:Lazy build markdown-preview.nvim` once
+return {
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
+}
